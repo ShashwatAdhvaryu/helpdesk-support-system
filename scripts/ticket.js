90cbeb958f55126066212fda2765e7a0686b89e4
ptlmeet27 <20124040@nuv.ac.in>
2022-10-16 12:46:10 +0530

    style

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,9 +1,5 @@
 
 
-    var database = firebase.database();
-    var ref = database.ref('ticketRecord');
-
-
 var ticketsArray = [];
 
 function newTicket(){
@@ -12,7 +8,7 @@ function newTicket(){
     outerDiv.className='container';
     let div = document.createElement('div');
     div.className = 'jumbotron';
-    div.style ="background-color:NavajoWhite";
+    div.style ="background-color:#f5f3ef";
     let form = document.createElement('form');
     form.id ='newTicketForm';
     form.innerHTML ='<h1>ADD NEW TICKET</h1>';
@@ -155,8 +151,8 @@ function newTicket(){
 
     let divSubmit = document.createElement('div');
     divSubmit.type = 'button';
-    divSubmit.className ='btn btn-success';
-    divSubmit.innerHTML='SUBMIT';
+    divSubmit.className ='submit-btn';
+    divSubmit.innerHTML='Submit';
     divSubmit.addEventListener("click",ticketSubmit);
 
     outerDiv.appendChild(div);
@@ -173,52 +169,7 @@ function newTicket(){
     document.body.appendChild(outerDiv);
 }
 
-// function generateTicketNumber(){
-//     let chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
-//     let nums = "0123456789";
-//     let string_length = 3;
-//     let number_length = 2;
-//     let randomstring = '';
-//     let randomnumber = '';
-//     for (let i=0; i<string_length; i++) {
-//         let rnum = Math.floor(Math.random() * chars.length);
-//         randomstring += chars.substring(rnum,rnum+1);
 
-//     }
-//     for (let i=0; i<number_length; i++) {
-//         let r = Math.floor(Math.random() * nums.length);
-//         randomnumber += nums.substring(r,r+1);
-
-//     }
-//         return  (randomstring + randomnumber);
-
-// }
-// function randomTableColor(){
-//     let x = Math.floor(Math.random()*5);
-//      let colors = ["table-primary","table-success","table-danger","table-info","table-warning","table-light"];
-//               return colors[x];
-
-// }
-
-// function ticketSummary(ticketNumber,ticketTitle,ticketDepartment,ticketPriority,ticketCustomerName){
-
-//     let table = document.getElementById("ticketTableBody");
-//     let row = table.insertRow();
-//     row.className = randomTableColor();
-//     let ticketNumberCell = row.insertCell(0);
-//     let ticketTitleCell = row.insertCell(1);
-//     let ticketDepartmentCell = row.insertCell(2);
-//     let ticketPriorityCell = row.insertCell(3);
-//     let ticketCustomerCell = row.insertCell(4);
-//     let ticketAction = row.insertCell(5);
-
-//     ticketNumberCell.innerHTML = ticketNumber;
-//     ticketTitleCell.innerHTML = ticketTitle;
-//     ticketDepartmentCell.innerHTML = ticketDepartment;
-//     ticketPriorityCell.innerHTML = ticketPriority;
-//     ticketCustomerCell.innerHTML = ticketCustomerName;
-//     ticketAction.innerHTML= '<button class="btn-info">View Details</button>';
-// }
 function ticketSubmit(){
 
     let ticketNumber = generateTicketNumber();
@@ -264,104 +215,3 @@ function resetNewTicketDiv(){
 
 }
 
-//function init(){
-
-    // ref.once('value',function(snapshot){
-
-    //     snapshot.forEach(function (item){
-    //        let ticketRecord = (JSON.stringify(item.val()));
-    //        let records = JSON.parse(ticketRecord);
-    //       ticketsArray.push(records);
-    //         ticketSummary(item.val().ticketNumber, item.val().title, item.val().department, item.val().priority, item.val().customerName);
-    //     })
-
-    // });
-//}
-
-/**function loadSummary(){
-    for (let i = 0; i < ticketsArray.length; i++) {
-        ticketSummary(ticketsArray[i].ticketNumber, ticketsArray[i].title, ticketsArray[i].department, ticketsArray[i].priority, ticketsArray[i].customerName);
-    }
-}
-
-
-/**function summaryTicket(){
-
-    loadTicketSummary();
-
-    for (let i = 0; i < ticketsArray.length; i++){
-        ticketSummary(ticketsArray[i].ticketNumber, ticketsArray[i].title, ticketsArray[i].department, ticketsArray[i].priority, ticketsArray[i].customerName);
-    }
-}**/
-
-
-
-
-
-/**
-// get the table element
-var $table = document.getElementById("ticketTable"),
-// number of rows per page
-    $n = 10,
-// number of rows of the table
-    $rowCount = $table.rows.length,
-// get the first cell's tag name (in the first row)
-    $firstRow = $table.rows[0].firstElementChild.tagName,
-// boolean var to check if table has a head row
-    $hasHead = ($firstRow === "TH"),
-// an array to hold each row
-    $tr = [],
-// loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
-    $i,$ii,$j = ($hasHead)?1:0,
-// holds the first row if it has a (<TH>) & nothing if (<TD>)
-    $th = ($hasHead?$table.rows[(0)].outerHTML:"");
-// count the number of pages
-var $pageCount = Math.ceil($rowCount / $n);
-// if we had one page only, then we have nothing to do ..
-if ($pageCount > 1) {
-    // assign each row outHTML (tag name & innerHTML) to the array
-    for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
-        $tr[$ii] = $table.rows[$i].outerHTML;
-    // create a div block to hold the buttons
-    $table.insertAdjacentHTML("afterend","<div id='buttons'></div>");
-    // the first sort, default page is the first one
-    sort(1);
-}
-
-// ($p) is the selected page number. it will be generated when a user clicks a button
-function sort($p) {
-    /* create ($rows) a variable to hold the group of rows
-    ** to be displayed on the selected page,
-    ** ($s) the start point .. the first row in each page, Do The Math
-
-    var $rows = $th,$s = (($n * $p)-$n);
-    for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
-        $rows += $tr[$i];
-
-    // now the table has a processed group of rows ..
-    $table.innerHTML = $rows;
-    // create the pagination buttons
-    document.getElementById("buttons").innerHTML = pageButtons($pageCount,$p);
-    // CSS Stuff
-    document.getElementById("id"+$p).setAttribute("class","active");
-}
-
-
-
-// ($pCount) : number of pages,($cur) : current page, the selected one ..
-function pageButtons($pCount,$cur) {
-    /** this variables will disable the "Prev" button on 1st page
-       and "next" button on the last one */
-   // var	$prevDis = ($cur == 1)?"disabled":"",
-    //    $nextDis = ($cur == $pCount)?"disabled":"",
-        /* this ($buttons) will hold every single button needed
-        ** it will creates each button and sets the onclick attribute
-        ** to the "sort" function with a special ($p) number..
-
-        $buttons = "<input type='button' value='&lt;&lt; Prev' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
-    for ($i=1; $i<=$pCount;$i++)
-        $buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
-    $buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
-    return $buttons;
-}
-**/
